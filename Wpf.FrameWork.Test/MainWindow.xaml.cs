dd8d1c7216b3d8271f80b03c07ec6d5af4d8209c
tp1415926535 <iamhermit@163.com>
2024-03-27 13:48:15 +0800

    AddTestProject

---
+++ Wpf.FrameWork.Test/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf.FrameWork.Test
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MainWindowViewModel viewModel;
+        public MainWindow()
+        {
+            InitializeComponent();
+            viewModel = new MainWindowViewModel();
+            this.DataContext = viewModel;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(string.Join(",", TagSelect.Result) + Environment.NewLine + string.Join(",", viewModel.ResultList));
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            viewModel.AddItem();
+        }
+    }
+}
---
+++ Wpf.FrameWork.Test/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Wpf.FrameWork.Test
+{
+    class MainWindowViewModel
+    {
+        public ObservableCollection<string> SourceList { get; set; } = new ObservableCollection<string>()
+        {
+            "data1","data2","data3","data4"
+        };
+
+
+        public ObservableCollection<string> ResultList { get; set; } = new ObservableCollection<string>()
+        { "data2" };
+
+
+        public void AddItem()
+        {
+            ResultList.Add("data1");
+            ResultList.Add("dataCustom");
+        }
+
+        public void GetResult()
+        {
+            MessageBox.Show(string.Join(",", ResultList));
+        }
+    }
+}
